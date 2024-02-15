9e7e82acad3fc8cf2846f377a01ad172c81ad7b7
BT_1N3_23 <jasonl375@nycstudents.net>
2024-02-15 12:33:16 -0500

    koolaid

--- Algorm.java
+++ Algorm.java
@@ -1,11 +1,19 @@
 import java.util.ArrayList;
 public class Algorm {
-    public void removeDupCon (ArrayList<Integer> ints){
-        for(int i = 0 ; i < ints.size() ; i++)
+//    public void removeDupCon (ArrayList<Integer> ints){
+//        for(int i = 0 ; i < ints.size() ; i++)
+//        {
+//            if (ints.get(i) == ints.get(i +1)){
+//                ints.remove(i);
+//            }
+//        }
+//    }
+    public String shortestString (ArrayList<String> strings)
+    {
+        String shortest = "";
+        for(String s: strings)
         {
-            if (ints.get(i) == ints.get(i +1)){
-                ints.remove(i);
-            }
+
         }
     }
 }
